ff608b8801f3a57372016ff076697de073325baf
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2015-11-18 16:40:35 +0100

    Fail fast middleware not activated by default.

--- appengine.go
+++ appengine.go
@@ -52,6 +52,36 @@ func ContextTimeout(ctx context.Context, w http.ResponseWriter, r *http.Request,
 	panic("should not reach here")
 }
 
+// ContextTimeoutFailFast sets a shorter timeout in the context of interactive requests and allows
+// the previous middleware to log or answer request timeouts. Be sure you process the
+// "request timeout" error or it will panic when finishing the middleware stack. It is not activated
+// by deault; but you can clear the middleware stack and put it instead of ContextTimeout().
+func ContextTimeoutFailFast(ctx context.Context, w http.ResponseWriter, r *http.Request, next NextMiddlewareFn) error {
+	secs := 15
+	if r.Header.Get("X-AppEngine-QueueName") != "" || r.Header.Get("X-AppEngine-Cron") != "" {
+		secs = 9*60 + 55
+	}
+
+	ctx, cancel := context.WithDeadline(ctx, time.Now().Add(secs*time.Second))
+
+	done := make(chan error, 1)
+	go func() {
+		done <- next(ctx)
+	}()
+
+	select {
+	case err := <-done:
+		cancel()
+		return err
+
+	case <-time.After(secs * time.Second):
+		cancel()
+		return errors.New("request timeout")
+	}
+
+	panic("should not reach here")
+}
+
 // LoggerMiddleware logs to Cloud Logging the error.
 func LoggerMiddleware(ctx context.Context, w http.ResponseWriter, r *http.Request, next NextMiddlewareFn) error {
 	if err := next(); err != nil {
