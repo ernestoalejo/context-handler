470ac378a65cc926df8c626093816fc979392a09
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2015-11-18 16:44:17 +0100

    Fix build issues.

--- appengine.go
+++ appengine.go
@@ -26,12 +26,12 @@ func ContextMiddleware(ctx context.Context, w http.ResponseWriter, r *http.Reque
 
 // ContextTimeout sets a timeout in the context.
 func ContextTimeout(ctx context.Context, w http.ResponseWriter, r *http.Request, next NextMiddlewareFn) error {
-	secs := 55
+	secs := 55 * time.Second
 	if r.Header.Get("X-AppEngine-QueueName") != "" || r.Header.Get("X-AppEngine-Cron") != "" {
-		secs = 9*60 + 55
+		secs = (9*60 + 55) * time.Second
 	}
 
-	ctx, cancel := context.WithDeadline(ctx, time.Now().Add(secs*time.Second))
+	ctx, cancel := context.WithDeadline(ctx, time.Now().Add(secs))
 
 	done := make(chan error, 1)
 	go func() {
@@ -43,7 +43,7 @@ func ContextTimeout(ctx context.Context, w http.ResponseWriter, r *http.Request,
 		cancel()
 		return err
 
-	case <-time.After(secs * time.Second):
+	case <-time.After(secs):
 		log.Criticalf(ctx, "request timeout")
 		cancel()
 		return nil
@@ -57,12 +57,12 @@ func ContextTimeout(ctx context.Context, w http.ResponseWriter, r *http.Request,
 // "request timeout" error or it will panic when finishing the middleware stack. It is not activated
 // by deault; but you can clear the middleware stack and put it instead of ContextTimeout().
 func ContextTimeoutFailFast(ctx context.Context, w http.ResponseWriter, r *http.Request, next NextMiddlewareFn) error {
-	secs := 15
+	secs := 15 * time.Second
 	if r.Header.Get("X-AppEngine-QueueName") != "" || r.Header.Get("X-AppEngine-Cron") != "" {
-		secs = 9*60 + 55
+		secs = (9*60 + 55) * time.Second
 	}
 
-	ctx, cancel := context.WithDeadline(ctx, time.Now().Add(secs*time.Second))
+	ctx, cancel := context.WithDeadline(ctx, time.Now().Add(secs))
 
 	done := make(chan error, 1)
 	go func() {
@@ -74,7 +74,7 @@ func ContextTimeoutFailFast(ctx context.Context, w http.ResponseWriter, r *http.
 		cancel()
 		return err
 
-	case <-time.After(secs * time.Second):
+	case <-time.After(secs):
 		cancel()
 		return errors.New("request timeout")
 	}
@@ -84,7 +84,7 @@ func ContextTimeoutFailFast(ctx context.Context, w http.ResponseWriter, r *http.
 
 // LoggerMiddleware logs to Cloud Logging the error.
 func LoggerMiddleware(ctx context.Context, w http.ResponseWriter, r *http.Request, next NextMiddlewareFn) error {
-	if err := next(); err != nil {
+	if err := next(ctx); err != nil {
 		log.Errorf(ctx, "%s", errors.ErrorStack(err))
 	}
 
@@ -93,7 +93,7 @@ func LoggerMiddleware(ctx context.Context, w http.ResponseWriter, r *http.Reques
 
 // ClientErrorMiddleware rejects the request with a 500 status code when an error occurs.
 func ClientErrorMiddleware(ctx context.Context, w http.ResponseWriter, r *http.Request, next NextMiddlewareFn) error {
-	err := next()
+	err := next(ctx)
 	if err != nil {
 		http.Error(w, "handler error", http.StatusInternalServerError)
 	}
