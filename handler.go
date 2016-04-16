17f4417dd078ac7237778425e852eb69c49da6bc
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2016-04-16 19:11:48 +0200

    Expose app own buffered response to write middlewares.

--- handler.go
+++ handler.go
@@ -12,7 +12,8 @@ type CtxHandler func(ctx context.Context, w http.ResponseWriter, r *http.Request
 
 type handlerFunc func(ctx context.Context, w http.ResponseWriter, r *http.Request) error
 
-type appResponseWriter struct {
+// AppResponseWriter the package uses to cache the responses. Exposed to be able to build or own middlewares.
+type AppResponseWriter struct {
 	statusCode int
 	buffer     *bytes.Buffer
 	header     http.Header
@@ -20,12 +21,12 @@ type appResponseWriter struct {
 }
 
 // Header will call the same function in the standard response writer.
-func (w *appResponseWriter) Header() http.Header {
+func (w *AppResponseWriter) Header() http.Header {
 	return w.header
 }
 
 // Write will call the same function in the standard response writer.
-func (w *appResponseWriter) Write(data []byte) (n int, err error) {
+func (w *AppResponseWriter) Write(data []byte) (n int, err error) {
 	if !w.written {
 		w.statusCode = http.StatusOK
 		w.written = true
@@ -35,13 +36,13 @@ func (w *appResponseWriter) Write(data []byte) (n int, err error) {
 }
 
 // WriteHeader will call the same function in the standard response writer.
-func (w *appResponseWriter) WriteHeader(statusCode int) {
+func (w *AppResponseWriter) WriteHeader(statusCode int) {
 	w.statusCode = statusCode
 	w.written = true
 }
 
 // Reset removes any content in the memory buffer for the response.
-func (w *appResponseWriter) Reset() {
+func (w *AppResponseWriter) Reset() {
 	w.buffer.Reset()
 	w.header = make(http.Header)
 }
@@ -49,7 +50,7 @@ func (w *appResponseWriter) Reset() {
 // Ctx adapts a context handler to the standard HTTP lib contract.
 func Ctx(fn CtxHandler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		wbuf := &appResponseWriter{
+		wbuf := &AppResponseWriter{
 			buffer: bytes.NewBuffer(nil),
 			header: make(http.Header),
 		}
